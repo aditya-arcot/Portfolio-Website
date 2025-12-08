06cd5c9ba2550fc5806d7a52069e98c99a7d971e
aditya-arcot <dev@aarcot.com>
2025-12-08 14:13:49 -0600

    upgrade angular

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import { provideZoneChangeDetection } from '@angular/core'
 import { bootstrapApplication } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
 import { appConfig } from './app/app.config'
 
-// eslint-disable-next-line no-console
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
+/* eslint-disable no-console */
+bootstrapApplication(AppComponent, {
+    ...appConfig,
+    providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((err) => console.error(err))
+/* eslint-enable no-console */
