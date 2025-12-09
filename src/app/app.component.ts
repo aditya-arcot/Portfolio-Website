809cc73b0a940ec98a59e3dfc602fdd7b124a123
aditya-arcot <dev@aarcot.com>
2025-12-09 09:32:30 -0600

    display image tag version in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { env } from 'src/environment'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        // eslint-disable-next-line no-console
+        console.info(`Image tag: ${env.imageTag}`)
+    }
+}
---
+++ src/environment.ts
@@ -0,0 +1,3 @@
+export const env = {
+    imageTag: '{IMAGE_TAG}',
+}
